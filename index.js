7288084de2b94a2ddfce4dfbefc253865dcb5829
Naomi Kudren <N.Kudren@amaze.com>
2017-01-31 17:10:41 +0000

    adding blinds

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ $(document).ready(function() {
     $("#pullMe").click(function() {
         $("#blindsString").addClass("pulled");
         $("#pullMe").hide();
+        $("#blindsDown").addClass("down");
+        $(".blindsCollection").hide();
         setTimeout(function() {
             $("#blindsString").removeClass("pulled");
             $("#pullMe").show();
+            $("#blindsDown").css("height", "40vh");
         }, 2000);
+
+
     });
+
 
 });
