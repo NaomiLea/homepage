1cf657d9bf843e9bc99283880e996cee6675fc4d
Naomi Kudren <N.Kudren@amaze.com>
2017-02-01 11:37:14 +0000

    adding working blinds

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ $(document).ready(function() {
     $("#pullMe").click(function() {
         $("#blindsString").addClass("pulled");
         $("#pullMe").hide();
-        $("#blindsDown").addClass("down");
-        $(".blindsCollection").hide();
+
+        if (!$("#blindsDown").hasClass("down")) {
+
+            $("#blindsDown").addClass("down");
+            $("#blindsDown").removeClass("up");
+            $(".blindsCollection").hide();
+            setTimeout(function() {
+                $("#blindsDown").css("height", "40vh");
+            }, 2000);
+        } else {
+            $("#blindsDown").removeClass("down");
+            $("#blindsDown").addClass("up");
+
+            setTimeout(function() {
+                $(".blindsCollection").show();
+                $("#blindsDown").css("height", "0vh");
+            }, 2000);
+        }
+
         setTimeout(function() {
             $("#blindsString").removeClass("pulled");
             $("#pullMe").show();
-            $("#blindsDown").css("height", "40vh");
         }, 2000);
 
 
